983dbc62a03eab5974eb7f2a5701c022f4735abc
ldesio <35573086+ldesio@users.noreply.github.com>
2018-10-07 13:40:43 +0200

    Added license

--- app/outfile.R
+++ app/outfile.R
@@ -10,48 +10,15 @@
 
 
 
-#' <div class='panel panel-primary'><div class='panel-heading'><h3 class='panel-title'> use docs/example.dta </h3></div><div class='panel-body'>
+#' <div class='panel panel-primary'><div class='panel-heading'><h3 class='panel-title'> update test </h3></div><div class='panel-body'>
 #+ results='asis', echo=FALSE
 source(file="cmd.rm/_star_lib.R")
-star.cmdline <- "use docs/example.dta"
-cmd <- star_parse_stata_command("filename")
-#syntax filename
-#POSTPROCESS update_vars
+star.cmdline <- "update test"
+cmd <- star_parse_stata_command("anything")
+#syntax anything
 
-if (cmd$filename=="xx") cmd$filename <- file.choose()
-
-
-#+ results='asis', echo=FALSE 
-cat(paste("Opening dataset <span class='label label-default'>",cmd$filename,"</span>",sep=""))
-# cat(paste("Opening dataset **",cmd$filename,"**",sep=""))
-
-#+ results='asis', echo=FALSE 
-library(foreign)
-	
-# Detaching and removing any previous "star.data" object
-tryCatch({ 
-	detach(star.data)
-  rm(star.data)
-	}
-  , error = function(err) {cat(" ")}, warning = function(err) {cat(" ")}
-)
-
-# Loading dataset, registering the "star.data" object in the global scope (double arrow), attaching.
-tryCatch({ 
-  star.data <<- read.dta(cmd$filename)
-	attach(star.data)
-	cat(paste("\r\n\r\n<span class='label label-success'>",ncol(star.data),"</span> variables, <span class='label label-success'>",nrow(star.data),"</span> observations.",sep=""))
-}, error = function(err) {errorPanel(err)}, warning = function(err) {warningPanel(err)}
-)
-
-
-
-
-
-
-# + results='asis', echo=FALSE 
-
-	
-	
+enforce_packages(c("RCurl"))
+library(RCurl)
+pandoc.p("Update.")
 
 #' </div></div>
