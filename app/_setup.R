c8862a8b47349c108062a1def2b8f8485fb9351f
Giuseppe Carteny <giuseppe.carteny@unimi.it>
2021-02-05 11:22:38 +0800

    Added 'lm.beta' package to 'setup.R'

--- app/_setup.R
+++ app/_setup.R
@@ -1,9 +1,5 @@
-# list.of.packages <- c("plumber", "foreign", "knitr", "summarytools","expss","stargazer")
-# new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-# if(length(new.packages)) install.packages(new.packages)
-want = c("plumber", "tidyverse", "foreign", "knitr", "summarytools", "expss", "haven", "stargazer")
+want = c("plumber", "tidyverse", "foreign", "knitr", "summarytools", "expss", "haven", "stargazer", "factors", "lm.beta")
 have = want %in% rownames(installed.packages())
 if ( any(!have) ) { install.packages( want[!have] ) }
 # junk = lapply(want, library, character.only = TRUE)
-# rm(have,want,junk)
-rm(have,want)
+rm(list=ls())
