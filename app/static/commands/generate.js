0deeb38783c0d610fa73e5714c713a16e352a376
Giuseppe Carteny <giuseppe.carteny@unimi.it>
2021-02-05 12:11:44 +0800

    'generate' and 'replace' now behave properly

--- app/static/commands/generate.js
+++ app/static/commands/generate.js
@@ -1,16 +1,63 @@
-/*GC 2021-01-07 
-It is identical to 'replace', what this command needs is a conditional statement in JavaScript that checks 
-whether or not the 'input.newvar' does exist in the original dataset. If the var to be replaced DOES EXIST, 
-then it should return an error message.*/
+if (currentCommandLine.includes("if")===false) {
+
+  input = parseStataSyntaxFromCommandLine({parseType:"gen"});
+  
+  if (datasetHasVariable(input.newvar) === false) {
+    
+  loadDataset({
+  	input:input,
+  	command: datasetUse,
+  	postProcess: "library(dplyr);\n" +
+  		"stardata <- mutate(.data=stardata, " + input.newvar + " = " + input.expression +");" + 
+  		"var_lab(stardata$" + input.newvar + ") = '" + input.expression +"';"
+  });
+  
+  appendContent("Variable generated successfully.");
+    
+  } else {
+    /* 
+    GC 2021-01-08 I tried existing funs to report the error but I have not been able to 
+    use those in this passage, thus I just created an ad hoc one.
+    */
+    appendCommandDescription(input.commandline);
+    appendContent("<span style='color:red'> Variable <b>" + input.newvar + "</b> already exists.</span>");
+  }
+  
+} else {
+  
+  input = parseStataSyntaxFromCommandLine({parseType:"gen"});
+  
+  if (datasetHasVariable(input.newvar) === false) {
+    
+    var ifcond = currentCommandLine.substr(currentCommandLine.indexOf('if')+2).trim();
+    
+    loadDataset({
+    	input:input,
+    	command: datasetUse,
+    	postProcess: "library(dplyr);\n" +
+    	  "stardata2 <- subset(x=stardata, c(" + ifcond + "));" +
+    		"stardata2 <- mutate(.data=stardata2, " + input.newvar + " = " + input.expression +");" + 
+    		"stardata <- left_join(stardata, stardata2);" +
+    		"rm(stardata2);" +
+    		"var_lab(stardata$" + input.newvar + ") = '" + input.expression + " (" + ifcond + ")';"
+    });  
+    
+    appendContent("Variable generated successfully.");
+    
+  } else {
+        /* 
+    GC 2021-01-08 I tried existing funs to report the error but I have not been able to 
+    use those in this passage, thus I just created an ad hoc one.
+    */
+    appendCommandDescription(input.commandline);
+    appendContent("<span style='color:red'> Variable <b>" + input.newvar + "</b> already exists.</span>");
+  }
+}
 
 
-input = parseStataSyntaxFromCommandLine({parseType:"gen"});
 
-loadDataset({
-	input:input,
-	command: datasetUse,
-	postProcess: "library(dplyr);\n" +
-		"stardata <- mutate(.data=stardata, " + input.newvar + " = " + input.expression +");" + 
-		"var_lab(stardata$" + input.newvar + ") = '" + input.expression +"';"
-});
-appendContent("Variable generated successfully.");
+
+
+
+  
+
--- app/static/commands/replace.js
+++ app/static/commands/replace.js
@@ -1,16 +1,56 @@
-/*GC 2021-01-07 
-It is identical to 'generate', what this command needs is a conditional statement in JavaScript that checks 
-whether or not the 'input.newvar' does exist in the original dataset. If the var to be replaced DOES NOT EXIST, 
-then it should return an error message.*/
+if (currentCommandLine.includes("if")===false) {
 
-
-input = parseStataSyntaxFromCommandLine({parseType:"gen"});
-
-loadDataset({
-	input:input,
-	command: datasetUse,
-	postProcess: "library(dplyr);\n" +
-		"stardata <- mutate(.data=stardata, " + input.newvar + " = " + input.expression +");" + 
-		"var_lab(stardata$" + input.newvar + ") = '" + input.expression +"';"
-});
-appendContent("Variable replaced successfully.");
+  input = parseStataSyntaxFromCommandLine({parseType:"gen"});
+  
+  if (datasetHasVariable(input.newvar) === true) {
+    
+  loadDataset({
+  	input:input,
+  	command: datasetUse,
+  	postProcess: "library(dplyr);\n" +
+  		"stardata <- mutate(.data=stardata, " + input.newvar + " = " + input.expression +");" + 
+  		"var_lab(stardata$" + input.newvar + ") = '" + input.expression +"';"
+  });
+  
+  appendContent("Variable replaced successfully.");
+    
+  } else {
+    /* 
+    GC 2021-01-08 I tried existing funs to report the error but I have not been able to 
+    use those in this passage, thus I just created an ad hoc one.
+    */
+    appendCommandDescription(input.commandline);
+    appendContent("<span style='color:red'> Variable <b>" + input.newvar + "</b> does not exist.</span>");
+  }
+  
+} else {
+  
+  input = parseStataSyntaxFromCommandLine({parseType:"gen"});
+  
+  if (datasetHasVariable(input.newvar) === true) {
+    
+    var ifcond = currentCommandLine.substr(currentCommandLine.indexOf('if')+2).trim();
+    
+    loadDataset({
+    	input:input,
+    	command: datasetUse,
+    	postProcess: "library(dplyr);\n" +
+    	  "stardata2 <- subset(x=stardata, c(" + ifcond + "));" +
+    		"stardata2 <- mutate(.data=stardata2, " + input.newvar + " = " + input.expression +");" + 
+    		"stardata <- dplyr::select(.data=stardata, -c(" + input.newvar + "));" +
+    		"stardata <- left_join(stardata, stardata2);" +
+    		"rm(stardata2);" +
+    		"var_lab(stardata$" + input.newvar + ") = '" + input.expression + " (" + ifcond + ")';"
+    });  
+    
+    appendContent("Variable replaced successfully.");
+    
+  } else {
+    /* 
+    GC 2021-01-08 I tried existing funs to report the error but I have not been able to 
+    use those in this passage, thus I just created an ad hoc one.
+    */
+    appendCommandDescription(input.commandline);
+    appendContent("<span style='color:red'> Variable <b>" + input.newvar + "</b> does not exist.</span>");
+  }
+}
