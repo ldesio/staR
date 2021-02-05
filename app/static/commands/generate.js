565a901958060a130285cda0b68142d1cbd8a182
Giuseppe Carteny <giuseppe.carteny@unimi.it>
2021-02-05 12:06:48 +0800

    Added comments for LDS on 'replace.js' and 'generate.js'

--- app/static/commands/generate.js
+++ app/static/commands/generate.js
@@ -1,3 +1,9 @@
+/*GC 2021-01-07 
+It is identical to 'replace', what this command needs is a conditional statement in JavaScript that checks 
+whether or not the 'input.newvar' does exist in the original dataset. If the var to be replaced DOES EXIST, 
+then it should return an error message.*/
+
+
 input = parseStataSyntaxFromCommandLine({parseType:"gen"});
 
 loadDataset({
