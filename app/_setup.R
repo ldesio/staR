f0c5ec20dc4b376e737041bbaabf9bc016ba5bcf
Giuseppe Carteny <giuseppe.carteny@unimi.it>
2021-02-07 14:56:21 +0800

    Changes in '_setup.R'
    Now the setup includes 'magrittr' pkg, used to implement 'replace.js'

--- app/_setup.R
+++ app/_setup.R
@@ -1,4 +1,6 @@
-want = c("plumber", "tidyverse", "foreign", "knitr", "summarytools", "expss", "haven", "stargazer", "factors")
+want = c("plumber", "tidyverse", "foreign", "knitr", "summarytools", "expss", "haven", "stargazer", "labelled", "magrittr")
+# "factors", 
+# non-existing package name: it could be either "lfactors" or "forcats"
 have = want %in% rownames(installed.packages())
 if ( any(!have) ) { install.packages( want[!have] ) }
 # junk = lapply(want, library, character.only = TRUE)
