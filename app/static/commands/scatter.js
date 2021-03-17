6b2d3f13306c77cdb7c7e972e2126b989f4cdb89
ldesio <35573086+ldesio@users.noreply.github.com>
2021-03-17 21:28:37 +0100

    Added "jitter" option to "scatter"

--- app/static/commands/scatter.js
+++ app/static/commands/scatter.js
@@ -1,13 +1,16 @@
-input = parseStataSyntaxFromCommandLine({parseType:"varlist"});
+input = parseStataSyntaxFromCommandLine({parseType:"varlist", options:["jitter"]});
+
+var theseVars = "nolabeled$" + input.vars[1] + ", nolabeled$" + input.vars[0];
+if (input.options.jitter) theseVars = "jitter(nolabeled$" + input.vars[1] + "), jitter(nolabeled$" + input.vars[0] +")";
+
+
 runPackage({
 	title: "Scatterplot of " + input.vars[0] + " by " + input.vars[1],
 	input:input,
 	rCommands: [
 		"nolabeled <- data.frame(data.matrix(stardata));",
 		wrapGraphicCommand(
-			"plot(nolabeled$" + input.vars[1] + 
-			",nolabeled$" + input.vars[0] + 
-			", xlab='" + Encoder.htmlEncode(varLabel(input.vars[1]))+"', ylab='" + Encoder.htmlEncode(varLabel(input.vars[0]))+"');"
+			"plot(" + theseVars + ", xlab='" + Encoder.htmlEncode(varLabel(input.vars[1]))+"', ylab='" + Encoder.htmlEncode(varLabel(input.vars[0]))+"');"
 		)
 	]
 })
