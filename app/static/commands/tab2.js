8f167836a33c15459bbc4a93b18d097eafb3c13d
ldesio <35573086+ldesio@users.noreply.github.com>
2021-03-17 10:16:25 +0100

    Update tab2.js
    Added always total row and column;
    Empty rows and columns now removed by default;
    "missing" option enabled, shows all rows/columns

--- app/static/commands/tab2.js
+++ app/static/commands/tab2.js
@@ -1,24 +1,27 @@
-input = parseStataSyntaxFromCommandLine({parseType:"varlist", options:["nofreq", "row", "column", "cell"]});
+input = parseStataSyntaxFromCommandLine({parseType:"varlist", options:["nofreq", "row", "column", "cell", "missing"]});
 
 rCommands = [
 	"cat(\"<h3>Cross-tabulation of " + input.vars[0] + " by " + input.vars[1] + "</h3>\");"
 	]
 
+	var _droprc = "drop_rc";
+	if (input.options.missing) _droprc = "";
+
 	if (!input.options.nofreq) {
 		rCommands.push("cat(\"<h4>Absolute frequencies</h4>\")");
-		rCommands.push("cat(knit_print(cro(stardata$" + input.vars[0] + ", stardata$" + input.vars[1] + ",total_label=\"N\"))[1]);");
+		rCommands.push("cat(knit_print(" + _droprc + "(cro(stardata$" + input.vars[0] + ", list(stardata$" + input.vars[1] + ",total()),total_label=\"N\")))[1]);");
 	}
 	if (input.options.row) {
 		rCommands.push("cat(\"<h4>Row percentages</h4>\")");
-		rCommands.push("cat(knit_print(cro_rpct(stardata$" + input.vars[0] + ", list(stardata$" + input.vars[1] + ",total()),total_label=\"N\",total_row_position=\"none\"))[1]);"); 
+		rCommands.push("cat(knit_print(" + _droprc + "(cro_rpct(list(stardata$" + input.vars[0] + ",total()), list(stardata$" + input.vars[1] + ",total()),total_label=\"N\",total_row_position=\"none\")))[1]);"); 
 	}
 	if (input.options.column) {
 		rCommands.push("cat(\"<h4>Column percentages</h4>\")");
-		rCommands.push("cat(knit_print(cro_cpct(list(stardata$" + input.vars[0] + ",total()), stardata$" + input.vars[1] + ",total_label=\"N\",total_row_position=\"none\"))[1]);"); 
+		rCommands.push("cat(knit_print(" + _droprc + "(cro_cpct(list(stardata$" + input.vars[0] + ",total()), list(stardata$" + input.vars[1] + ",total()),total_label=\"N\",total_row_position=\"none\")))[1]);"); 
 	}
 	if (input.options.cell) {
 		rCommands.push("cat(\"<h4>Cell percentages</h4>\")");
-		rCommands.push("cat(knit_print(cro_tpct(list(stardata$" + input.vars[0] + ",total()), list(stardata$" + input.vars[1] + ",total()),total_label=\"N\",total_row_position=\"none\"))[1]);"); 
+		rCommands.push("cat(knit_print(" + _droprc + "(cro_tpct(list(stardata$" + input.vars[0] + ",total()), list(stardata$" + input.vars[1] + ",total()),total_label=\"N\",total_row_position=\"none\")))[1]);"); 
 	}
 
 runPackage({
