37cdfbcee6ab15ec955c43f0e733829fb6a93b63
ldesio <35573086+ldesio@users.noreply.github.com>
2021-04-11 13:24:24 +0200

    Added basic support for weights in tab1 and tab2
    The whole weight clause is passed to the command JS, which is free to process it as it likes.
    
    Currently, tab1 and tab2 do not distinguish between different Stata weights (aw, iw, etc.), and pass the variable after the "=" in thw weight clause as "weight" option to expss::fre() and expss::cro()

--- app/static/commands/tab1.js
+++ app/static/commands/tab1.js
@@ -8,15 +8,22 @@ if  (input.options.nolabel) noLabel = true;
 
 rCommands.push("options(huxtable.knitr_output_format = \"html\")");
 
+console.log(input.weight);
+var freWeight = "", weightDesc = "";
+if (input.weight) {
+	freWeight = ", weight=stardata$" + input.weight.split("=")[1];
+	weightDesc = ";<br>[data weighted by <i>" + input.weight.split("=")[1] + "</i>]";
+}
+
 for (i=0; i < input.vars.length; i++) {
 	let name = input.vars[i], label = varLabel(input.vars[i]);
 	label = Encoder.htmlEncode(label);
 	
 	rCommands.push(
-		"cat(paste0('<h3>Frequency distribution of " + name + " (<i>" + label + "</i>)</h3>'))");
+		"cat(paste0('<h3>Frequency distribution of " + name + " (<i>" + label + "</i>)"+weightDesc+"</h3>'))");
 	if (noLabel) rCommands.push("val_lab(stardata$"+name+")=NULL");
 
-	rCommands.push("cat(knit_print(fre(stardata$" + name + ")))");
+	rCommands.push("cat(knit_print(fre(stardata$" + name + freWeight + ")))");
 	if (i<input.vars.length-1) rCommands.push("\"<br/>\"");
 }
 
--- app/static/commands/tab2.js
+++ app/static/commands/tab2.js
@@ -1,7 +1,14 @@
 input = parseStataSyntaxFromCommandLine({parseType:"varlist", options:["nofreq", "row", "column", "cell", "missing"]});
 
+var freWeight = "", weightDesc = "";
+if (input.weight) {
+	freWeight = ", weight=stardata$" + input.weight.split("=")[1];
+	weightDesc = ";<br>[data weighted by <i>" + input.weight.split("=")[1] + "</i>]";
+}
+
+
 rCommands = [
-	"cat(\"<h3>Cross-tabulation of " + input.vars[0] + " by " + input.vars[1] + "</h3>\");"
+	"cat(\"<h3>Cross-tabulation of " + input.vars[0] + " by " + input.vars[1] + weightDesc + "</h3>\");"
 	]
 
 	var _droprc = "drop_rc";
@@ -9,19 +16,19 @@ rCommands = [
 
 	if (!input.options.nofreq) {
 		rCommands.push("cat(\"<h4>Absolute frequencies</h4>\")");
-		rCommands.push("cat(knit_print(" + _droprc + "(cro(stardata$" + input.vars[0] + ", list(stardata$" + input.vars[1] + ",total()),total_label=\"N\")))[1]);");
+		rCommands.push("cat(knit_print(" + _droprc + "(cro(stardata$" + input.vars[0] + freWeight + ", list(stardata$" + input.vars[1] + ",total()),total_label=\"N\")))[1]);");
 	}
 	if (input.options.row) {
 		rCommands.push("cat(\"<h4>Row percentages</h4>\")");
-		rCommands.push("cat(knit_print(" + _droprc + "(cro_rpct(list(stardata$" + input.vars[0] + ",total()), list(stardata$" + input.vars[1] + ",total()),total_label=\"N\",total_row_position=\"none\")))[1]);"); 
+		rCommands.push("cat(knit_print(" + _droprc + "(cro_rpct(list(stardata$" + input.vars[0] + freWeight + ",total()), list(stardata$" + input.vars[1] + ",total()),total_label=\"N\",total_row_position=\"none\")))[1]);"); 
 	}
 	if (input.options.column) {
 		rCommands.push("cat(\"<h4>Column percentages</h4>\")");
-		rCommands.push("cat(knit_print(" + _droprc + "(cro_cpct(list(stardata$" + input.vars[0] + ",total()), list(stardata$" + input.vars[1] + ",total()),total_label=\"N\",total_row_position=\"none\")))[1]);"); 
+		rCommands.push("cat(knit_print(" + _droprc + "(cro_cpct(list(stardata$" + input.vars[0] + freWeight + ",total()), list(stardata$" + input.vars[1] + ",total()),total_label=\"N\",total_row_position=\"none\")))[1]);"); 
 	}
 	if (input.options.cell) {
 		rCommands.push("cat(\"<h4>Cell percentages</h4>\")");
-		rCommands.push("cat(knit_print(" + _droprc + "(cro_tpct(list(stardata$" + input.vars[0] + ",total()), list(stardata$" + input.vars[1] + ",total()),total_label=\"N\",total_row_position=\"none\")))[1]);"); 
+		rCommands.push("cat(knit_print(" + _droprc + "(cro_tpct(list(stardata$" + input.vars[0] + freWeight + ",total()), list(stardata$" + input.vars[1] + ",total()),total_label=\"N\",total_row_position=\"none\")))[1]);"); 
 	}
 
 runPackage({
