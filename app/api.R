7b51e3dd6de2010ee73519b47cd33ef220f6989a
Lorenzo De Sio <35573086+ldesio@users.noreply.github.com>
2021-06-18 16:36:02 +0200

    Update api.R
    Removed commented functions

--- app/api.R
+++ app/api.R
@@ -14,18 +14,6 @@ function(command) {
   toJSON(result, force = TRUE)
 }
 
-
-# NOW USES /mutatedata
-# #* Load data return JSON
-# #* @get /loaddata
-# #* @post /loaddata
-#function(command) {
-#  stardata <- eval(parse(text=command))
-#  uniqueID <- as.numeric(Sys.time())*1000;
-#  fileName <- paste0(uniqueID,".Rdata");
-#  toJSON(list(stardata,attributes(stardata), fileName), force = TRUE)
-#}
-
 #* Load/Mutate data, return JSON
 #* @get /mutatedata
 #* @post /mutatedata
@@ -45,30 +33,3 @@ function(command, postprocess="") {
               fileName), force = TRUE)
 }
 
-
-
-#* Process Command and return PNG URL in JSON
-#* @get /cmdoutputPNGURL
-#* @post /cmdoutputPNGURL
-#function(command) {
-#  tmp_filename <- paste0(format(Sys.time(), format = '%Y-%j-%H%M%S'),'.png')
-#  png(paste0('static/',tmp_filename))
-#  eval(parse(text=command))
-#  dev.off()
-#  imgtag <- paste0('<img src=',tmp,'>')
-#  toJSON(imgtag, force = TRUE)
-#}
-
-#* Process Command and return PNG URL in JSON
-#* @get /pngwithdata
-#* @post /pngwithdata
-#function(data, command) {
-#  tmp_filename <- paste0(format(as.numeric(Sys.time())*1000,digits=15),'.png');
-#  
-#  eval(parse(text=data))
-#  png(paste0('static/generated/',tmp_filename))
-#  eval(parse(text=command))
-#  dev.off()
-#  imgtag <- paste0('<img src=generated/',tmp_filename,'>')
-#  toJSON(imgtag, force = TRUE)
-#}
